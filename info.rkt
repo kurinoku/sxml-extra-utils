d565be1f1e231396cf178da88ec43c6fbe8e5ac4
kurinoku <31811692+kurinoku@users.noreply.github.com>
2021-12-04 15:46:58 -0300

    updated version

--- info.rkt
+++ info.rkt
@@ -6,6 +6,6 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" ))
 (define scribblings '(("scribblings/sxml-extra.scrbl" ())))
 (define pkg-desc "Description Here")
-(define version "0.1.0")
+(define version "0.1")
 (define pkg-authors '(kurinoku))
 (define license '(MIT))
